43284f1e2b9b1b54029d1b40b961528f1314190c
Yogita Kumar <61360028+yogitakumar@users.noreply.github.com>
2021-06-03 17:53:04 +0100

    BubbleSort and InsertionSort

---
+++ BubbleSort/BubbleSort/BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BubbleSort");
+            int[] arr = { 6, 2, 8, 3, 1, 9 };
+            foreach(int x in arr)
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine("\n------------------");
+            for(int i = 0; i < arr.Length - 1; i++) 
+            {
+                for(int j=0;j<arr.Length-i-1;j++)
+                {
+                    
+                    if(arr[j]>arr[j+1])
+                    {
+                        int temp = arr[j + 1];
+                        arr[j+1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+                Console.WriteLine("\nPass : " + i);
+                for (int p = 0; p < arr.Length; p++)
+                {
+                    
+                    Console.Write(arr[p] + " ");
+                }
+            }
+            Console.WriteLine("\nsorted array");
+            foreach (int x in arr)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
---
+++ InsertionSort/InsertionSort/Insertion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Insertion
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insertion Sort");
+            int[] arr = { 5, 1, 8, 2, 9, 3, 6, 0 };
+            foreach(int x in arr)
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine("\n--------------------");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int temp = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+                Console.WriteLine("\nPass : " + i);
+                foreach(int x in arr)
+                {
+                    Console.Write(x + " ");
+                }
+            }
+            Console.WriteLine("\nSorted Array");
+            foreach (int x in arr)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
